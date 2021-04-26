bc550ebc1f379e2751b404ce1f6756aa2f76250d
niuzs-alan <zhisheng.niu@jina.ai>
2021-04-26 14:41:43 +0800

    doc(version): remove the version in readme

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@
  * ```hcl
  * module "jinad" {
  *    source         = "jina-ai/jinad-aws/jina"
- *    version        = "0.0.6"
  *    instances      = {
  *      encoder: {
  *        type: "c5.4xlarge"
